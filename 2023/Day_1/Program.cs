a78352463f09626c92d6dd30515aaa3ac0982835
Thomas Diggs <thomasdiggs@gmail.com>
2024-09-24 21:12:41 -0600

    Day 1, Part One in C# complete

--- 2023/Day_1/Program.cs
+++ 2023/Day_1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Day_1
+﻿using System.Globalization;
+
+namespace Day_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char first = input[i].First(x => x >= 48 && x <= 57);   // 48 is decimal for char '0'
+                char last = input[i].Last(x =>  x >= 48 && x <= 57);    // 57 is decimal for char '9'
+                sum += Int32.Parse(string.Concat(first, last));
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
